8402805b16722c60cb809a661eeb77854bf8e811
Russell Brown <russell@wombat.me>
2024-12-01 21:29:28 +0100

    fuck

--- src/main.rs
+++ src/main.rs
@@ -14,34 +14,37 @@ fn main() {
                 println!("accepted new connection");
                 let mut ms = [0u8; 4];
                 let _ = stream.read_exact(&mut ms).unwrap();
-                let len = u32::from_be_bytes(ms);
+                let len = i32::from_be_bytes(ms);
                 let mut buff = vec![0; (len - 4) as usize];
                 stream.read_exact(&mut buff).unwrap();
 
-                let _api = u16::from_be_bytes([buff[0], buff[1]]);
-                let _req_ver = u16::from_be_bytes([buff[2], buff[3]]);
+                let _api = i16::from_be_bytes([buff[0], buff[1]]);
+                let _req_ver = i16::from_be_bytes([buff[2], buff[3]]);
 
                 let cid = [buff[4], buff[5], buff[6], buff[7]];
-                let error_code = 0u16.to_be_bytes();
-                let key_count = 2u32.to_be_bytes(); // why??
+                let error_code = 0i16.to_be_bytes();
+                let key_count = 1i32.to_be_bytes();
                 let api_key =
-                    [18u16.to_be_bytes(), 0u16.to_be_bytes(), 4u16.to_be_bytes()].concat(); //is min 4 or zero?
-
+                    [18i16.to_be_bytes(), 4i16.to_be_bytes(), 4i16.to_be_bytes()].concat(); //is min 4 or zero?
                 let mess_len = cid.len()
                     + error_code.len()
                     + key_count.len()
                     + api_key.len()
-                    + size_of::<u32>();
+                    + size_of::<i32>()
+                    + size_of::<i8>();
 
                 let mut resp = Vec::with_capacity(mess_len);
 
-                resp.extend((mess_len as u32).to_be_bytes());
+                resp.extend((mess_len as i32).to_be_bytes());
                 resp.extend(cid);
                 resp.extend(error_code);
                 resp.extend(key_count);
                 resp.extend(api_key);
+                resp.extend(0i8.to_be_bytes());
                 println!("{:?}", resp);
-                let res = stream.write_all(&resp);
+                let _ = stream.write_all(&resp);
+
+                let res = stream.flush();
 
                 println!("res {:?}", res);
             }
